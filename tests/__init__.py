a718a75b1387fb49125889a5be2e8a150ec30fe2
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-07-16 11:34:14 +0200

    typing
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,7 @@
+import json
 from pathlib import Path
+from typing import TypedDict,  Required, NotRequired
+
 
 TESTS_ROOT_DIR = Path(__file__).parent
 TESTS_DATA_DIR = TESTS_ROOT_DIR / '_data'
@@ -7,3 +10,21 @@ TESTS_DATA_FILES = {
     'invalid_syntax': TESTS_DATA_DIR / 'invalid_syntax.json',
     'valid_syntax': TESTS_DATA_DIR / 'valid_syntax.json',
 }
+
+class SyntaxCase(TypedDict):
+    value: Required[str]
+    reason: Required[str]
+    expect_lark: NotRequired[bool]
+    expect_regex: NotRequired[bool]
+    conforms_with_rfc3987_semantics: NotRequired[bool]
+    semantics_notes: NotRequired[str]
+
+T_syntax_file = dict[str, list[SyntaxCase]]
+
+def valid_syntax_data() -> T_syntax_file:
+    with open(TESTS_DATA_FILES['valid_syntax'], "r", encoding="utf-8") as f:
+        return json.load(f)
+
+def invalid_syntax_data() -> T_syntax_file:
+    with open(TESTS_DATA_FILES['invalid_syntax'], "r", encoding="utf-8") as f:
+        return json.load(f)
