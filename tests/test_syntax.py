4043ef247be87ad88606c9f1a3e9c92d8d013729
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-07-16 11:42:24 +0200

    tests: typing (#15)
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/test_syntax.py
+++ tests/test_syntax.py
@@ -1,29 +1,21 @@
-from pathlib import Path
-import json
+from typing import TYPE_CHECKING
+
 import pytest
+from typing import Callable
 
 import rfc3987_syntax2 as sut
 
-from . import TESTS_DATA_FILES
-
-def valid_syntax_data():
-    with open(TESTS_DATA_FILES['valid_syntax'], "r", encoding="utf-8") as f:
-        return json.load(f)
+from . import valid_syntax_data, invalid_syntax_data, SyntaxCase, T_syntax_file
 
 
-def invalid_syntax_data():
-    with open(TESTS_DATA_FILES['invalid_syntax'], "r", encoding="utf-8") as f:
-        return json.load(f)
-
-
-def syntax_data_as_params(src_cb):
+def syntax_data_as_params(src_cb: Callable[[], T_syntax_file]):
     for term, examples in src_cb().items():
         for example in examples:
             yield pytest.param(term, example, id=f"{term}-{example['value']}")
 
 
 @pytest.mark.parametrize("term,valid_example", syntax_data_as_params(valid_syntax_data))
-def test_is_valid_syntax(term, valid_example):
+def test_is_valid_syntax(term: str, valid_example: SyntaxCase) -> None:
     actual = sut.is_valid_syntax(term=term, value=valid_example["value"])
     print("")
     print(
@@ -33,8 +25,9 @@ def test_is_valid_syntax(term, valid_example):
         actual is True
     ), f"Failed term: {term} for '{valid_example['value']}' : {valid_example['reason']}"
 
+
 @pytest.mark.parametrize("term,invalid_example", syntax_data_as_params(invalid_syntax_data))
-def test_not_is_valid_syntax(term, invalid_example):
+def test_not_is_valid_syntax(term: str, invalid_example: SyntaxCase) -> None:
     actual = sut.is_valid_syntax(term=term, value=invalid_example["value"])
     print("")
     print(
