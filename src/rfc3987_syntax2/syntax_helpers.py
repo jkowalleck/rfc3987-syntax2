e2b657cd368ba7754210818b315fbdf37e92881b
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-07-20 13:40:42 +0200

    lsazy-load
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- src/rfc3987_syntax2/syntax_helpers.py
+++ src/rfc3987_syntax2/syntax_helpers.py
@@ -2,7 +2,7 @@
 # Copyright (c) 2026 Jan Kowalleck - modifications and maintenance
 # SPDX-License-Identifier: MIT
 
-from typing import Callable
+from typing import Callable, Optional, TYPE_CHECKING
 
 from lark import Lark, ParseTree, exceptions
 
@@ -59,6 +59,7 @@ __all__ = [
     "is_valid_syntax_port",
 ]
 
+
 RFC3987_SYNTAX_PARSER_TYPE: str = "earley"
 RFC3987_SYNTAX_GRAMMAR_PATH: Path = Path(__file__).parent / "syntax_rfc3987.lark"
 RFC3987_SYNTAX_TERMS: list[str] = [
@@ -105,13 +106,8 @@ RFC3987_SYNTAX_TERMS: list[str] = [
     "pct_encoded",
 ]
 
-grammar: str = load_grammar(RFC3987_SYNTAX_GRAMMAR_PATH)
-
-syntax_parser = Lark(grammar, start=["iri", "iri_reference", "absolute_iri"], parser=RFC3987_SYNTAX_PARSER_TYPE)
-
-
 def parse(term: str, value: str) -> ParseTree:
-    return syntax_parser.parse(value, start=term)
+    return get_syntax_parser().parse(value, start=term)
 
 
 def is_valid_syntax(term: str, value: str) -> bool:
@@ -121,9 +117,12 @@ def is_valid_syntax(term: str, value: str) -> bool:
     except exceptions.LarkError:
         return False
 
+T_SYNTAX_VALIDATOR = Callable[[str], bool]
 
-def make_syntax_validator(rule_name: str) -> Callable[[str], bool]:
-    parser = Lark(grammar, start=rule_name, parser=RFC3987_SYNTAX_PARSER_TYPE)
+def make_syntax_validator(rule_name: str) -> T_SYNTAX_VALIDATOR:
+    parser = Lark(get_grammar(),
+                  start=rule_name,
+                  parser=RFC3987_SYNTAX_PARSER_TYPE)
 
     def syntax_validator(text: str) -> bool:
         try:
@@ -135,78 +134,96 @@ def make_syntax_validator(rule_name: str) -> Callable[[str], bool]:
     return syntax_validator
 
 
-is_valid_syntax_iri = make_syntax_validator("iri")
+_grammar: Optional[str] = None
 
-is_valid_syntax_iri_reference = make_syntax_validator("iri_reference")
 
-is_valid_syntax_absolute_iri = make_syntax_validator("absolute_iri")
+def get_grammar() -> str:
+    """this is private API"""
+    global _grammar
+    if _grammar is None:
+        _grammar = load_grammar(RFC3987_SYNTAX_GRAMMAR_PATH)
+    return _grammar
 
-is_valid_syntax_irelative_ref = make_syntax_validator("irelative_ref")
 
-is_valid_syntax_irelative_part = make_syntax_validator("irelative_part")
+_syntax_parser: Optional[Lark] = None
 
-is_valid_syntax_ihier_part = make_syntax_validator("ihier_part")
 
-is_valid_syntax_iauthority = make_syntax_validator("iauthority")
+def get_syntax_parser() -> Lark:
+    """this is private API"""
+    global _syntax_parser
+    if _syntax_parser is None:
+        _syntax_parser = Lark(get_grammar(),
+                              start=["iri", "iri_reference", "absolute_iri"],
+                              parser=RFC3987_SYNTAX_PARSER_TYPE)
+    return _syntax_parser
 
-is_valid_syntax_iuserinfo = make_syntax_validator("iuserinfo")
 
-is_valid_syntax_ihost = make_syntax_validator("ihost")
+_CACHE_SYNTAX_VALIDATOR: dict[str, T_SYNTAX_VALIDATOR] = {}
 
-is_valid_syntax_ireg_name = make_syntax_validator("ireg_name")
 
-is_valid_syntax_ipath = make_syntax_validator("ipath")
+def get_syntax_validator(rule_name) -> T_SYNTAX_VALIDATOR:
+    """this is private API"""
+    validator = _CACHE_SYNTAX_VALIDATOR.get(rule_name)
+    if validator is None:
+        validator = _CACHE_SYNTAX_VALIDATOR[rule_name] = make_syntax_validator(rule_name)
+    return validator
 
-is_valid_syntax_ipath_abempty = make_syntax_validator("ipath_abempty")
 
-is_valid_syntax_ipath_absolute = make_syntax_validator("ipath_absolute")
+if TYPE_CHECKING:
+    # define types for all those lazy-loaded symbols
+    grammar: str
+    syntax_parser: Lark
+    is_valid_syntax_iri: T_SYNTAX_VALIDATOR
+    is_valid_syntax_iri_reference: T_SYNTAX_VALIDATOR
+    is_valid_syntax_absolute_iri: T_SYNTAX_VALIDATOR
+    is_valid_syntax_irelative_ref: T_SYNTAX_VALIDATOR
+    is_valid_syntax_irelative_part: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ihier_part: T_SYNTAX_VALIDATOR
+    is_valid_syntax_iauthority: T_SYNTAX_VALIDATOR
+    is_valid_syntax_iuserinfo: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ihost: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ireg_name: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipath: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipath_abempty: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipath_absolute: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipath_noscheme: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipath_rootless: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipath_empty: T_SYNTAX_VALIDATOR
+    is_valid_syntax_isegment: T_SYNTAX_VALIDATOR
+    is_valid_syntax_isegment_nz: T_SYNTAX_VALIDATOR
+    is_valid_syntax_isegment_nz_nc: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipchar: T_SYNTAX_VALIDATOR
+    is_valid_syntax_iquery: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ifragment: T_SYNTAX_VALIDATOR
+    is_valid_syntax_iunreserved: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ucschar: T_SYNTAX_VALIDATOR
+    is_valid_syntax_iprivate: T_SYNTAX_VALIDATOR
+    is_valid_syntax_sub_delims: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ip_literal: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipvfuture: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipv6address: T_SYNTAX_VALIDATOR
+    is_valid_syntax_h16: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ls32: T_SYNTAX_VALIDATOR
+    is_valid_syntax_ipv4address: T_SYNTAX_VALIDATOR
+    is_valid_syntax_dec_octet: T_SYNTAX_VALIDATOR
+    is_valid_syntax_unreserved: T_SYNTAX_VALIDATOR
+    is_valid_syntax_alpha: T_SYNTAX_VALIDATOR
+    is_valid_syntax_digit: T_SYNTAX_VALIDATOR
+    is_valid_syntax_hexdig: T_SYNTAX_VALIDATOR
+    is_valid_syntax_port: T_SYNTAX_VALIDATOR
 
-is_valid_syntax_ipath_noscheme = make_syntax_validator("ipath_noscheme")
 
-is_valid_syntax_ipath_rootless = make_syntax_validator("ipath_rootless")
+def __getattr__(name: str):
+    if name not in __all__:
+        raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+    if name == 'grammar':
+        return get_grammar()
+    if name == 'syntax_parser':
+        return get_syntax_parser()
+    if name.startswith('is_valid_syntax_'):
+        return get_syntax_validator(name[16:])  # the prefix is 16 chars long
+    raise NotImplementedError(f"module {__name__!r} failed to implement attribute {name!r}")
 
-is_valid_syntax_ipath_empty = make_syntax_validator("ipath_empty")
 
-is_valid_syntax_isegment = make_syntax_validator("isegment")
-
-is_valid_syntax_isegment_nz = make_syntax_validator("isegment_nz")
-
-is_valid_syntax_isegment_nz_nc = make_syntax_validator("isegment_nz_nc")
-
-is_valid_syntax_ipchar = make_syntax_validator("ipchar")
-
-is_valid_syntax_iquery = make_syntax_validator("iquery")
-
-is_valid_syntax_ifragment = make_syntax_validator("ifragment")
-
-is_valid_syntax_iunreserved = make_syntax_validator("iunreserved")
-
-is_valid_syntax_ucschar = make_syntax_validator("ucschar")
-
-is_valid_syntax_iprivate = make_syntax_validator("iprivate")
-
-is_valid_syntax_sub_delims = make_syntax_validator("sub_delims")
-
-is_valid_syntax_ip_literal = make_syntax_validator("ip_literal")
-
-is_valid_syntax_ipvfuture = make_syntax_validator("ipvfuture")
-
-is_valid_syntax_ipv6address = make_syntax_validator("ipv6address")
-
-is_valid_syntax_h16 = make_syntax_validator("h16")
-
-is_valid_syntax_ls32 = make_syntax_validator("ls32")
-
-is_valid_syntax_ipv4address = make_syntax_validator("ipv4address")
-
-is_valid_syntax_dec_octet = make_syntax_validator("dec_octet")
-
-is_valid_syntax_unreserved = make_syntax_validator("unreserved")
-
-is_valid_syntax_alpha = make_syntax_validator("alpha")
-
-is_valid_syntax_digit = make_syntax_validator("digit")
-
-is_valid_syntax_hexdig = make_syntax_validator("hexdig")
-
-is_valid_syntax_port = make_syntax_validator("port")
+def __dir__() -> list[str]:
+    return sorted(list(globals().keys()) + __all__)
