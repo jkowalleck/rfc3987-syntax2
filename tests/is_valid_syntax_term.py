08050854ab73ecd732c53d95ec3bc3f1b3516c69
Jan Kowalleck <jan.kowalleck@gmail.com>
2026-07-23 14:25:30 +0200

    wip
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- tests/is_valid_syntax_term.py
+++ tests/is_valid_syntax_term.py
@@ -6,7 +6,8 @@ from typing import Any, Callable
 
 import pytest
 
-import rfc3987_syntax2 as sut
+import rfc3987_syntax2.is_valid_syntax_term as sut
+from rfc3987_syntax2 import RFC3987_SYNTAX_TERMS
 
 from . import SyntaxCase, T_syntax_file, invalid_syntax_data, valid_syntax_data
 
@@ -39,3 +40,20 @@ def test_not_is_valid_syntax(term: str, invalid_example: SyntaxCase) -> None:
     assert (
         actual is False
     ), f"Failed term: {term} for '{invalid_example['value']}' : {invalid_example['reason']}"
+
+
+
+@pytest.mark.parametrize("term", RFC3987_SYNTAX_TERMS)
+def test_term_validator_exists(term: str) -> None:
+    actual = sut.RFC3987_SYNTAX_TERM_VALIDATORS.get(term)
+    assert (actual is not None), f"missing func for {term}"
+
+
+@pytest.mark.parametrize("term", (t for t in RFC3987_SYNTAX_TERMS if t not in [
+    # non_zero - a local helper token used in `dec_octet`
+    'non_zero',
+]))
+def test_term_validator_as_expected(term: str) -> None:
+    expected = getattr(sut, f'is_valid_syntax_{term}')
+    actual = sut.RFC3987_SYNTAX_TERM_VALIDATORS[term]
+    assert (actual is expected), f"unexpected func for {term}"
